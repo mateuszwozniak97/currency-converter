db582ff1c8d50d1830d3b740abaf0548fe67ace4
Mateusz Woźniak <mw54544@gmail.com>
2023-04-03 10:25:06 +0200

    icon and og image added

--- JS/script.js
+++ JS/script.js
@@ -16,28 +16,28 @@
                 return amount / rateGBP;
             case "CNY":
                 return amount / rateCNY;
-        }
-    }
+        };
+    };
 
     const updateResultText = (amount, result, currency) => {
         const resultElement = document.querySelector(".js-result");
         resultElement.innerHTML = `<strong>${amount.toFixed(2)} PLN </strong> = <strong>${result.toFixed(2)} ${currency}</strong>`;
-    }
+    };
     const onFormSubmit = (event) => {
         event.preventDefault();
 
         const amountElement = document.querySelector(".js-amount");
-        const currencyElement = document.querySelector(".js-currency")
+        const currencyElement = document.querySelector(".js-currency");
 
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-        let result = calulateResult(+amount, currency)
+        let result = calulateResult(+amount, currency);
         updateResultText(amount, result, currency);
-    }
+    };
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
-        formElement.addEventListener("submit", onFormSubmit)
+        formElement.addEventListener("submit", onFormSubmit);
     };
     init();
-}
+};
